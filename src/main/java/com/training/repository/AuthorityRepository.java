7ddfc9d9ddf359e59f816dd4152513faf2e7e057
Thanh Bui Nguyen <thanh.buinguyen@trinity-technology.com>
2023-01-04 11:32:13 +0700

    Initial version of TrainingSource generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/training/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.training.repository;
+
+import com.training.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/training/service/InvalidPasswordException.java
@@ -0,0 +1,10 @@
+package com.training.service;
+
+public class InvalidPasswordException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public InvalidPasswordException() {
+        super("Incorrect password");
+    }
+}

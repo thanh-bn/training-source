b1a927cb7f24257cecad4b16adb58c42922b5d92
Le Dac Anh <dacanh159@gmail.com>
2023-01-05 15:57:52 +0700

    feature: create, update task, change state of task

--- src/main/java/com/training/web/rest/TaskResource.java
+++ src/main/java/com/training/web/rest/TaskResource.java
@@ -1,28 +1,47 @@
 package com.training.web.rest;
 
+import com.training.domain.Task;
+import com.training.domain.User;
+import com.training.repository.TaskRepository;
 import com.training.service.TaskService;
+import com.training.service.dto.AdminUserDTO;
 import com.training.service.dto.TaskDTO;
 import com.training.web.rest.errors.BadRequestAlertException;
+import com.training.web.rest.errors.EmailAlreadyUsedException;
+import com.training.web.rest.errors.InvalidTaskIdException;
+import com.training.web.rest.errors.LoginAlreadyUsedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import tech.jhipster.web.util.HeaderUtil;
+import tech.jhipster.web.util.ResponseUtil;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/tasks")
 public class TaskResource {
+
+    @Value("${jhipster.clientApp.name}")
+    private String applicationName;
+
     private final Logger logger = LoggerFactory.getLogger(TaskResource.class);
 
     private final TaskService taskService;
+    private final TaskRepository taskRepository;
 
-    public TaskResource(TaskService taskService) {
+    public TaskResource(TaskService taskService, TaskRepository taskRepository) {
         this.taskService = taskService;
+        this.taskRepository = taskRepository;
     }
 
     /**
@@ -66,8 +85,21 @@ public class TaskResource {
      * @throws BadRequestAlertException {@code 400 (Bad Request)} if fields constraints are not satisfied.
      */
     @PostMapping
-    public ResponseEntity<TaskDTO> createTask(@RequestBody TaskDTO taskDTO) {
-        return  null;
+    public ResponseEntity<Task> createTask(@Valid @RequestBody TaskDTO taskDTO) throws URISyntaxException {
+        logger.debug("REST request to save Task : {}", taskDTO);
+
+        if (taskDTO.getId() != null) {
+            throw new BadRequestAlertException("A new task cannot already have an ID", "taskManagement", "idexists");
+            // Lowercase the user login before comparing with database
+        }  else {
+            Task newTask = taskService.createTask(taskDTO);
+            return ResponseEntity
+                .created(new URI("/api/tasks/" + newTask.getId()))
+                .headers(
+                    HeaderUtil.createAlert(applicationName, "A task is created with identifier " + newTask.getId(), "" + newTask.getId())
+                )
+                .body(newTask);
+        }
     }
 
     /**
@@ -77,8 +109,19 @@ public class TaskResource {
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated task.
      */
     @PutMapping("/{id}")
-    public ResponseEntity<Object> updateTask(@RequestBody TaskDTO taskDTO) {
-        return null;
+    public ResponseEntity<TaskDTO> updateTask(@Valid @RequestBody TaskDTO taskDTO, @PathVariable("id") Long id) {
+        logger.debug("REST request to update Task : {}", taskDTO);
+        Optional<Task> existingTask = taskRepository.findById(taskDTO.getId());
+        if (existingTask.isPresent() && (!existingTask.get().getId().equals(id))) {
+            throw new InvalidTaskIdException();
+        }
+
+        Optional<TaskDTO> updatedTask = taskService.updateTask(taskDTO);
+
+        return ResponseUtil.wrapOrNotFound(
+            updatedTask,
+            HeaderUtil.createAlert(applicationName, "A task is updated with identifier " + taskDTO.getId(), "" + taskDTO.getId())
+        );
     }
 
     /**
@@ -89,8 +132,15 @@ public class TaskResource {
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated task.
      */
     @PatchMapping("/{id}")
-    public ResponseEntity<Object> changeState(@PathVariable("id") Long id, @RequestBody String state) {
-        return null;
+    public ResponseEntity<TaskDTO> changeState(@PathVariable("id") Long id, @RequestBody String state) {
+        logger.debug("REST request to change state of Task : {}", state);
+
+        Optional<TaskDTO> updatedTask = taskService.changeState(id, state);
+
+        return ResponseUtil.wrapOrNotFound(
+            updatedTask,
+            HeaderUtil.createAlert(applicationName, "A task is updated with identifier " + id, "" + id)
+        );
     }
 
     /**

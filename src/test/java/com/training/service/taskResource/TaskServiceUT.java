1b55f5efc71225a01d06817eab3d10d1dfc3629f
Le Dac Anh <dacanh159@gmail.com>
2023-01-06 09:25:49 +0700

    refactor TaskServiceUT name

--- src/test/java/com/training/service/taskResource/TaskServiceUT.java
+++ src/test/java/com/training/service/taskResource/TaskServiceUT.java
@@ -3,16 +3,13 @@ package com.training.service.taskResource;
 import com.training.repository.TaskRepository;
 import com.training.repository.UserRepository;
 import com.training.service.TaskService;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class TaskServiceTest {
+public class TaskServiceUT {
 
     @Mock
     private TaskRepository taskRepository;
